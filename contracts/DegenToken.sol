ea76a4629539c125017a9931edf6e15158f4f91f
Ayush Thakur <116510022+Ayush-Thakur-geek@users.noreply.github.com>
2024-07-28 12:21:20 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -2,28 +2,25 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract DegenToken is Ownable {
-    string public name = "Degen Token";
-    string public symbol = "DGN";
-    string private listItems =
-        "Items => 1 : armor : 10 | 2 : sword : 15 | 3 : elixor : 20";
+contract DegenToken is Ownable, ERC20 {
+    string public constant tokenName = "Degen Token";
+    string public constant tokenSymbol = "DGN";
+    string private listItems = "Items => 1 : armor : 10 | 2 : sword : 15 | 3 : elixor : 20";
     mapping(address => string[]) private inventory;
     mapping(uint => string) private items;
     mapping(string => uint) private itemPrices;
     mapping(address => uint) private toBeRedeemed;
-    mapping(address => uint) private balance;
-    uint private totalTokens = 0;
 
-    constructor(uint initialSupply) Ownable(msg.sender) {
+    constructor(uint256 initialSupply) ERC20(tokenName, tokenSymbol) Ownable(msg.sender) {
         items[1] = "armor";
         items[2] = "sword";
         items[3] = "elixor";
         itemPrices["armor"] = 10;
         itemPrices["sword"] = 15;
         itemPrices["elixor"] = 20;
-        mint(msg.sender, initialSupply);
+        _mint(msg.sender, initialSupply);
     }
 
     function store() public view returns (string memory) {
@@ -32,82 +29,41 @@ contract DegenToken is Ownable {
 
     function mint(address to, uint256 amount) public onlyOwner {
         toBeRedeemed[to] += amount;
-        totalTokens += amount;
     }
 
-    function totalSupply() public view returns (uint) {
-        return totalTokens;
-    }
-
-    function decimals() public pure returns (uint8) {
+    function decimals() public pure override returns (uint8) {
         return 0;
     }
 
-    function getBalance() external view returns (uint256) {
-        return balanceOf(msg.sender);
-    }
-
     function burn(uint256 amount) public {
-        require(balance[msg.sender] >= amount, "Not enough balance");
-        balance[msg.sender] -= amount;
-        totalTokens -= amount;
+        _burn(msg.sender, amount);
     }
 
     function burnFrom(address account, uint256 amount) public onlyOwner {
-        require(
-            balanceOf(account) >= amount,
-            "ERC20: burn amount exceeds balance"
-        );
-        balance[account] -= amount;
-        totalTokens -= amount;
-    }
-
-    function transfer(address to, uint value) public returns (bool) {
-        require(balanceOf(msg.sender) >= value, "Not enough balance");
-        balance[msg.sender] -= value;
-        balance[to] += value;
-        return true;
-    }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public onlyOwner returns (bool) {
-        require(
-            balanceOf(sender) >= amount,
-            "ERC20: transfer amount exceeds balance"
-        );
-        balance[sender] -= amount;
-        balance[recipient] += amount;
-        return true;
-    }
-
-    function balanceOf(address account) public view returns (uint256) {
-        return balance[account];
+        uint256 currentAllowance = allowance(account, msg.sender);
+        require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
+        _approve(account, msg.sender, currentAllowance - amount);
+        _burn(account, amount);
     }
 
     function purchaseItems(uint256 itemId) public {
         require(itemId > 0 && itemId < 4, "Item not found");
         string memory item = items[itemId];
-
-        require(
-            balanceOf(msg.sender) >= itemPrices[item],
-            "Insufficient balance"
-        );
-
+        uint256 itemPrice = itemPrices[item];
+        require(balanceOf(msg.sender) >= itemPrice, "Insufficient balance");
+        
+        _transfer(msg.sender, owner(), itemPrice);
         inventory[msg.sender].push(item);
-
-        transfer(owner(), itemPrices[item]);
     }
 
     function viewInventory() public view returns (string[] memory) {
         return inventory[msg.sender];
     }
 
-    function redeem() public returns (uint) {
-        require(toBeRedeemed[msg.sender] > 0, "Your redeem balance is zero");
-        balance[msg.sender] += toBeRedeemed[msg.sender];
-        return toBeRedeemed[msg.sender];
+    function redeem() public {
+        uint256 redeemAmount = toBeRedeemed[msg.sender];
+        require(redeemAmount > 0, "Your redeem balance is zero");
+        toBeRedeemed[msg.sender] = 0;
+        _mint(msg.sender, redeemAmount);
     }
 }
